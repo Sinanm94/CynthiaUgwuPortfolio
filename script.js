fe050a56fb136b3bc8530eac3c8842767f5d1214
Sinanm94 <122637646+Sinanm94@users.noreply.github.com>
2023-11-10 19:46:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,43 @@ const scroll = new LocomotiveScroll({
   smooth: true
 });
 
+function firstPageAnim(){
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y:'-10',
+    opacity:0,
+    duration:2,
+    ease: Expo.easeInOut
+  })
+  .to(".boundingelem", {
+    y:0,
+    ease: Expo.easeInOut,
+    duration:2,
+    delay:-1,
+    stagger:.2
+  })
+  .from("#herofooter", {
+    y:-10,
+    opacity:0,
+    duration:1.5,
+    delay:-1,
+    ease: Expo.easeInOut
+  })
+}
+
+function circleFade(){
+  window.addEventListener("mousemove", function(dets){
+
+  });
+}
+
+
 function circleMouseFollower(){
   window.addEventListener("mousemove", function(dets){
     document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
   })
 }
 
+firstPageAnim();
 circleMouseFollower();
